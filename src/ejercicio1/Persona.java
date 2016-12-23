ba43e32d46c2b7185271cf12539fdf269e63d9d2
Joaquin Vicente Alonso <joaalsai@ieslavereda.es>
2016-12-23 14:01:25 +0100

    Initial commit.

---
+++ src/ejercicio1/Persona.java
@@ -0,0 +1,15 @@
+package ejercicio1;
+
+public class Persona {
+	private String nombre;
+	private int edad;
+	private int DNI;
+	private char sexo;
+	
+	public Persona(){
+		 nombre	= "";
+		 edad	= 0;
+		 DNI	= 0;
+		 sexo	= 'H';
+	}
+}
