d188105be3ca02f6449642fdbab286a15cf278a9
Joaquin Vicente Alonso <joaalsai@ieslavereda.es>
2016-12-25 12:06:12 +0100

    Ejercicio3 con nuevas caracteristicas

--- src/ejercicio3/Ejercicio3.java
+++ src/ejercicio3/Ejercicio3.java
@@ -23,15 +23,24 @@ public class Ejercicio3 {
 					introducirLibro(libros);
 					break;
 				case 2:
-					prestarLibro(libros);
+					cambiarTitulo(libros);
 					break;
 				case 3:
-					devolverLibro(libros);
+					cambiarAutor(libros);
 					break;
 				case 4:
-					listarLibro(libros);
+					cambiarEjemplares(libros);
 					break;
 				case 5:
+					prestarLibro(libros);
+					break;
+				case 6:
+					devolverLibro(libros);
+					break;
+				case 7:
+					listarLibro(libros);
+					break;
+				case 8:
 					listarLibros(libros);
 					break;
 				case 0:
@@ -53,20 +62,21 @@ public class Ejercicio3 {
 		System.out.println("-----------------------------");
 		System.out.println("0- Salir");
 	}
-
 	public static void mostrarMenuPrincipal() {
 		System.out.println("      Introducir Libro       ");
 		System.out.println("-----------------------------");
-		System.out.println("1- Introducir libros");
-		System.out.println("2- Prestar libros");
-		System.out.println("3- Devolver libros");
-		System.out.println("4- Listar libro");
-		System.out.println("5- Listar todos los libros");
+		System.out.println("1- Introducir libro");
+		System.out.println("2- Cambiar titulo de libro");
+		System.out.println("3- Cambiar autor de libro");
+		System.out.println("4- Cambiar cantidad de ejemplares");
+		System.out.println("5- Prestar libros");
+		System.out.println("6- Devolver libros");
+		System.out.println("7- Consultar libro");
+		System.out.println("8- Listar todos los libros");
 		System.out.println("-----------------------------");
 		System.out.println("0- Salir");
 		System.out.println("-----------------------------");
 	}
-
 	public static void borrarPantalla() {
 		for (int i = 0; i < 100; i++) {
 			System.out.println();
@@ -89,6 +99,69 @@ public class Ejercicio3 {
 				libros[libroSeleccionado-1].setAutor(entrada.obtenerString("Dame el autor para el libro " + libros[libroSeleccionado-1].getTitulo()));
 				libros[libroSeleccionado-1].setNumeroDeEjemplares(entrada.obtenerEntero("Cuantos ejemplares hay para el libro " + libros[libroSeleccionado-1].getTitulo()));
 				libros[libroSeleccionado-1].setNumeroDeEjemplaresPrestados(0);
+			}
+		} while (!salir);
+	}
+	public static void cambiarTitulo(Libro[] libros){
+		Entrada entrada = new Entrada();
+		int libroSeleccionado;
+		boolean salir = false;
+		
+		do {
+			borrarPantalla();
+			mostrarSubMenu(libros);
+
+			libroSeleccionado = entrada.obtenerEntero("Que libro quiere modificar ?");
+			if(libroSeleccionado==0) salir=true;
+			else if (libroSeleccionado<0 || libroSeleccionado>libros.length){
+				entrada.obtenerString("Valor incorrecto!.\nDebes introducir un valor entre 0 y " + libros.length + "\n Pulsa [Intro] para continuar.");
+			} else {
+				borrarPantalla();
+				System.out.println("Libro seleccionado [" + libros[libroSeleccionado-1].getTitulo() +"].");
+				libros[libroSeleccionado-1].setTitulo(entrada.obtenerString("Introduce el nuevo titulo para el libro seleccionado:"));
+				entrada.obtenerString("Titulo del libro actualizado.\nPulsa [Intro] para continuar.");
+			}
+		} while (!salir);
+	}
+	public static void cambiarAutor(Libro[] libros){
+		Entrada entrada = new Entrada();
+		int libroSeleccionado;
+		boolean salir = false;
+		
+		do {
+			borrarPantalla();
+			mostrarSubMenu(libros);
+
+			libroSeleccionado = entrada.obtenerEntero("Que libro quiere modificar ?");
+			if(libroSeleccionado==0) salir=true;
+			else if (libroSeleccionado<0 || libroSeleccionado>libros.length){
+				entrada.obtenerString("Valor incorrecto!.\nDebes introducir un valor entre 0 y " + libros.length + "\n Pulsa [Intro] para continuar.");
+			} else {
+				borrarPantalla();
+				System.out.println("Libro seleccionado [" + libros[libroSeleccionado-1].getTitulo() +"].");
+				libros[libroSeleccionado-1].setAutor(entrada.obtenerString("Introduce el nuevo autor para el libro seleccionado:"));
+				entrada.obtenerString("Autor del libro actualizado.\nPulsa [Intro] para continuar.");
+			}
+		} while (!salir);
+	}
+	public static void cambiarEjemplares(Libro[] libros){
+		Entrada entrada = new Entrada();
+		int libroSeleccionado;
+		boolean salir = false;
+		
+		do {
+			borrarPantalla();
+			mostrarSubMenu(libros);
+
+			libroSeleccionado = entrada.obtenerEntero("Que libro quiere modificar ?");
+			if(libroSeleccionado==0) salir=true;
+			else if (libroSeleccionado<0 || libroSeleccionado>libros.length){
+				entrada.obtenerString("Valor incorrecto!.\nDebes introducir un valor entre 0 y " + libros.length + "\n Pulsa [Intro] para continuar.");
+			} else {
+				borrarPantalla();
+				System.out.println("Libro seleccionado [" + libros[libroSeleccionado-1].getTitulo() +"].");
+				libros[libroSeleccionado-1].setTitulo(entrada.obtenerString("Introduce la cantidad de ejemplares para el libro seleccionado:"));
+				entrada.obtenerString("Cantidad de ejemplares del libro actualizado.\nPulsa [Intro] para continuar.");
 			}
 		} while (!salir);
 	}
@@ -128,7 +201,7 @@ public class Ejercicio3 {
 				entrada.obtenerString("Valor incorrecto!.\nDebes introducir un valor entre 0 y " + libros.length + "\n Pulsa [Intro] para continuar.");
 			} else {
 				borrarPantalla();
-				if(libros[libroSeleccionado-1].prestamo()==true) System.out.println("Libro prestado correctamente!.");
+				if(libros[libroSeleccionado-1].devolucion()==true) System.out.println("Libro devuelto correctamente!.");
 				else System.out.println("El libro no puede ser devuelto. Consulte si hay prestamos.");
 				entrada.obtenerString("Pulsa [Intro] para continuar.");
 			}
